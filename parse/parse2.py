d90eb3adfdd83753c9c04b203c7168ef9bf3c910
Claudio Roman <itisclaudio@gmail.com>
2022-05-03 10:25:01 -0400

    settings timezone, parse2, commands

---
+++ parse/parse2.py
@@ -0,0 +1,21 @@
+import requests
+import lxml.html
+from datetime import date, datetime
+
+from parse.parseapp.models import Methodologies
+
+url='https://ejproxy.com/methodologies/'
+page = requests.get(url)
+page_content = lxml.html.fromstring(page.content)
+td_elements = page_content.xpath('//td')
+
+bulk_list = list()
+for t in range(0, len(td_elements), 2):
+	print("{} - {} - {}".format(t,td_elements[t].text_content().strip(),td_elements[t+1].text_content().strip()))
+	bulk_list.append(Methodologies(
+		title=td_elements[t].text_content().strip(),
+		dateadded = datetime.strptime(td_elements[t+1].text_content().strip(), "%B %d, %Y").date()
+		))
+Methodologies.objects.bulk_create(bulk_list)
+#bulk_msj = Methodologies.objects.bulk_create(bulk_list)
+#print(bulk_msj)
---
+++ parse/parseapp/management/commands/parse3.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+import requests
+import lxml.html
+from datetime import date, datetime
+
+from parse.parseapp.models import Methodologies
+
+class Command(BaseCommand):
+	help = 'Parses HTML table into Methodologies model'
+
+	def handle(self, *args, **options):
+	
+		url='https://ejproxy.com/methodologies/'
+		page = requests.get(url)
+		page_content = lxml.html.fromstring(page.content)
+		td_elements = page_content.xpath('//td')
+
+		bulk_list = list()
+		for t in range(0, len(td_elements), 2):
+			print("{} - {} - {}".format(t,td_elements[t].text_content().strip(),td_elements[t+1].text_content().strip()))
+			bulk_list.append(Methodologies(
+				title=td_elements[t].text_content().strip(),
+				dateadded = datetime.strptime(td_elements[t+1].text_content().strip(), "%B %d, %Y").date()
+				))
+		Methodologies.objects.bulk_create(bulk_list)
